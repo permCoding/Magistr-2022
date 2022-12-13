a0eab2214e351bd724651d07ef54e618fd655847
Андрей Беляков <ttxiom@gmail.com>
2022-12-13 12:15:55 +0500

    shedule

--- 01-Анализ и разработка алгоритмов/recursia/sort.js
+++ 01-Анализ и разработка алгоритмов/recursia/sort.js
@@ -1,9 +1,10 @@
 const comparator = (a, b, fields, directs) => {
     let dict = { 'asc': +1, 'desc': -1 };
-    let d = dict[directs[0]];
-    let f = fields[0];
+    let d = dict[directs[0]], f = fields[0];
+
+    // 1 точка останова - должна возвращать ответ
     if ((fields.length === 1) || (a[f] !== b[f])) {
-        return d * (a[f] > b[f]? +1: -1); // 1 точка останова - должна возвращать ответ
+        return d * (a[f] > b[f]? +1: -1); 
     }
     // 2 шаг рекурсии - вызов функции с оставшимися пар-ми для сортировки
     return comparator(a, b, fields.slice(1,), directs.slice(1,));
