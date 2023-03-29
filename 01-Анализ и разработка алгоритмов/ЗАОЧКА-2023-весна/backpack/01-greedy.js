c54aaf5a9bdb4797e86e957916d3a174088c4637
Андрей Беляков <ttxiom@gmail.com>
2023-03-29 20:38:28 +0500

    up

--- 01-Анализ и разработка алгоритмов/ЗАОЧКА-2023-весна/backpack/01-greedy.js
+++ 01-Анализ и разработка алгоритмов/ЗАОЧКА-2023-весна/backpack/01-greedy.js
@@ -2,14 +2,18 @@
 
 const get_greedy = (max_w, prods) => {
     let cur_w = 0, cur_p = 0
-    let i = prods.length // по какой индекс набрали предметов
-    // тут код жадного алгоритма
-    // отсортировать
-    //
-    //
-    //
-    //
-    //
+    let i = 0 // по какой индекс набрали предметов
+    
+    // отсортировать по убыванию
+    prods
+        .sort((a,b) => a.p/a.w > b.p/b.w? -1: +1)
+    while (true) {
+        if (cur_w + prods[i].w > max_w) break
+        cur_w += prods[i].w
+        cur_p += prods[i].p
+        i += 1
+    }
+
     return {
         'cur_w': cur_w,
         'cur_p': cur_p,
@@ -18,10 +22,16 @@ const get_greedy = (max_w, prods) => {
 }
 
 
-let prods = require('./json/input30.json')
-let max_w = 150
+let prods = require('./json/input6.json')
+let max_w = 1500
 
 // преобразовать типы данных в обектах
+prods
+    .map(item => { 
+        item.id = +item.id,
+        item.w = +item.w,
+        item.p = +item.p
+    })
 
 console.clear()
 console.log('greedy algorithm')
--- 01-Анализ и разработка алгоритмов/ЗАОЧКА-2023-весна/backpack/02-bin-mask.js
+++ 01-Анализ и разработка алгоритмов/ЗАОЧКА-2023-весна/backpack/02-bin-mask.js
@@ -3,28 +3,60 @@
 const get_bin_mask = (max_w, prods) => {
     let res_w = 0, res_p = 0
     let combo = 0 // тут будет индекс лучшей комбинации
-    // тут будет код алгоритма поиска бинарными масками
-    //
-    //
-    //
-    //
-    //
+    let n = 1 << prods.length
+    for (let i=0; i<n; i++) { // индекс комбинации
+        let cur_w = 0, cur_p = 0
+        for (let j=0; j<prods.length; j++) { // индекс предмета
+            mask = 1 << j
+            if (i & mask) {
+                cur_w += prods[j].w
+                cur_p += prods[j].p
+            }
+        }
+        if ((cur_w<=max_w) && (cur_p>res_p)) {
+            res_w = cur_w
+            res_p = cur_p
+            combo = i
+        }
+    }
+
     return {
         'res_w': res_w,
         'res_p': res_p,
-        'combo': combo.toString(2).split('').reverse().join('')
+        'combo': combo.toString(2).split('').reverse().join('') // это доработать
     }
 }
 
 
-let prods = require('./json/input20.json')
-let max_w = 100
+let prods = require('./json/input6.json')
+let max_w = 1500
 
-// преобразовать типы данных в обектах
+prods
+    .map(item => {
+        item.id = +item.id,
+        item.w = +item.w,
+        item.p = +item.p
+    })
 
 console.clear()
 console.log('bin mask algorithm')
 console.table(prods)
 
 let solver = get_bin_mask(max_w, prods)
-console.log(solver)
+console.log(JSON.stringify(solver, null, 4))
+
+/*
+2 ^ 4 === 1 << 4
+00001
+00010
+00100
+01000
+10000
+
+i = 13
+1101 <= j
+0001 <= mask0
+0010 <= mask1
+0100 <= mask2
+1000 <= mask3
+*/
