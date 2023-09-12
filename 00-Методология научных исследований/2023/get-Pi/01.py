ea405ef4a1e2d27704722c74cbcae2698296b20e
permCoding <ttxiom@gmail.com>
2023-09-12 22:39:20 +0500

    get_pi

--- 00-Методология научных исследований/2023/get-Pi/01.py
+++ 00-Методология научных исследований/2023/get-Pi/01.py
@@ -10,11 +10,23 @@ def get_pi(count):
     return 4*count_circle/count
 
 
-# def get_pi(eps=.001):
-    # while diff > eps:
-    #     next
-    # return result
+def get_pi_step(count):
+    count_circle = 0
+    for step in range(1, count+1):
+        x, y = random(), random()
+        if x**2 + y**2 <= 1:
+            count_circle += 1
+        if step%50_000 == 0:
+            print(step, 4*count_circle/step)
 
 
-n = int(input('Введите кол-во испытаний - '))
-print(f"Pi = {get_pi(n)}")
+# n = int(input('Введите кол-во испытаний - '))
+# print(f"Pi = {get_pi(n)}")
+get_pi_step(1_000_000)
+
+"""
+001_000_000 3.14_3148
+400_000_000 3.141_58231
+600_000_000 3.1415971066666666
+
+"""
