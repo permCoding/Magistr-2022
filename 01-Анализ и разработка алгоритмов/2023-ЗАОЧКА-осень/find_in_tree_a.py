bf97b60345107eec6ed5eab41b229c4c37989365
permCoding <ttxiom@gmail.com>
2023-09-27 23:16:19 +0500

    up

---
+++ 01-Анализ и разработка алгоритмов/2023-ЗАОЧКА-осень/find_in_tree_a.py
@@ -0,0 +1,51 @@
+from os import system
+
+
+def get_tree():
+    node5 = {
+        "key": 5,
+        "left": None,
+        "right": None
+    }
+    node3 = {
+        "key": 3,
+        "left": None,
+        "right": None
+    }
+    node7 = {
+        "key": 7,
+        "left": node5,
+        "right": None
+    }
+    tree = {
+        "key": 4,
+        "left": node3,
+        "right": node7
+    }
+    return tree
+
+
+def find_in_tree(node, finder_elm):
+    if node:  # node != None
+        if finder_elm == node["key"]:
+            return True
+        elif finder_elm < node["key"]:
+            return find_in_tree(node["left"], finder_elm)
+        else:
+            return find_in_tree(node["right"], finder_elm)
+    else:
+        return False
+
+
+system("clear")  # for Win - cls
+tree = get_tree()  # [4, 3, 7, 5]
+check_elements = [5,8,3,4,7]
+for elm in check_elements:
+    print(f"{elm} => {find_in_tree(tree, elm)}")
+
+"""
+функция get_tree - получить дерево 
+реализуйте самостоятельно
+на вход подаётся список - на выходе дерево
+можно использовать другой язык программирования
+"""
--- 01-Анализ и разработка алгоритмов/readme.md
+++ 01-Анализ и разработка алгоритмов/readme.md
@@ -10,6 +10,8 @@
 
 [Блокнот 1 - Ассимптотики](https://colab.research.google.com/drive/1Cwn_ZQjUEmln24_cfNLlTJ9AF8bZ0OOX?usp=sharing)  
 
+Найти файл со словами русского языка и подготовить задачу по нему...  
+
 ---  
 
 **Практические вопросы:**  
