07e8a073e0ee88bbfac2f8b933c81772b9c18fdf
permCoding <ttxiom@gmail.com>
2023-11-27 18:15:45 +0500

    up

---
+++ 01-Анализ и разработка алгоритмов/2023-ОЧКА-осень/08-parser/02-bs4/04-all_images/01.py
@@ -0,0 +1,36 @@
+import requests
+from fake_useragent import UserAgent
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    # ua = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0'
+    ua = UserAgent().chrome
+    head = {'User-Agent': ua}
+    resp = requests.get(url, headers=head)
+    resp.encoding = "utf8"
+    return resp.text
+
+
+def get_links_on_page(html):  # получить все ссылки на странице
+    soup = BeautifulSoup(html, 'html.parser')
+    arts = soup \
+        .find('div', class_='products-list') \
+        .find_all('article')
+    links = []
+    for item in arts:
+        link = item \
+            .find('a') \
+            .find('picture', {'class':'product-picture'}) \
+            .find('img')['data-src']
+        links.append(link)
+    return links
+
+
+host = 'https://www.chitai-gorod.ru/'
+cat = 'catalog/books/informatika-110299'
+url = host + cat
+html = get_html(url)
+links = get_links_on_page(html)
+
+for link in links: print(link.split('/')[-1], link)
