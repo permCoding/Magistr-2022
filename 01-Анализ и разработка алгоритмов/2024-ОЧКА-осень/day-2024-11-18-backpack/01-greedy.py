11889b0d4670b88d3c8865fab8f7c450a00936f3
Андрей Беляков <ttxiom@gmail.com>
2024-11-18 11:24:17 +0500

    up

---
+++ 01-Анализ и разработка алгоритмов/2024-ОЧКА-осень/day-2024-11-18-backpack/01-greedy.py
@@ -0,0 +1,28 @@
+class Greedy:
+    def __init__(self, capacity, items):
+        self.capacity = capacity
+        self.items = items  # исходная коллекция всех объектов
+        self.max_value = 0  # максимальная взятая ценность
+
+    def solve(self):
+        self.items.sort(key=lambda x: x["value"] / x["weight"], reverse=True)
+        weight, i = 0, 0
+        while True:
+            next_item = self.items[i]
+            if weight + next_item["weight"] > self.capacity: break
+            weight += next_item["weight"]
+            self.max_value += next_item["value"]
+            i += 1
+        return self.max_value, self.items[0: i]
+
+
+if __name__ == "__main__":
+    import json
+    with open('./json/input4.json') as file:
+        data = json.load(file)  # исходные данные
+    max_w = 100  # размер ранца
+    greedy = Greedy(max_w, data)
+    max_value, result = greedy.solve()  # ценность и список id объектов, взятых в рюкзак
+    print(f"Максимальная ценность рюкзака: {max_value}")
+    print("Объекты в рюкзаке:")
+    print(json.dumps(result, indent=4, ensure_ascii=False))
---
+++ 01-Анализ и разработка алгоритмов/2024-ОЧКА-осень/day-2024-11-18-backpack/04-queue.py
@@ -0,0 +1,36 @@
+class Backpack:
+    def __init__(self, capacity, items):
+        self.capacity = capacity
+        self.items = items  # исходная коллекция всех объектов
+        self.max_value = 0  # максимальная взятая ценность
+
+    def solve(self):
+        queue = []  # queue для хранения узлов
+        root = (0, 0, 0.0)  # (уровень, текущая ценность, текущий вес)
+        queue.append(root)
+        
+        while queue:  # пока очередь не пуста
+            current_level, current_value, current_weight = queue.pop(0)
+
+            if current_level < len(self.items):  # есть ли ещё объекты
+                next_item = self.items[current_level]  # следующий объект
+                # две ветви дерева перебора: 1. с включением и 2. без включением следующего объекта
+                if current_weight + next_item["weight"] <= self.capacity:  # не входит по весу
+                    new_value = current_value + next_item["value"]
+                    new_weight = current_weight + next_item["weight"]
+                    self.max_value = max(self.max_value, new_value)  # обновляем max_value
+                    queue.append((current_level + 1, new_value, new_weight))      # 1. добавляем в очередь
+                queue.append((current_level + 1, current_value, current_weight))  # 2. добавляем в очередь
+
+        return self.max_value  # result - список id объектов, взятых в рюкзак
+
+
+if __name__ == "__main__":
+    import json
+    with open('./json/input4.json') as file:
+        data = json.load(file)  # исходные данные
+    max_w = 91  # размер ранца
+    backpack = Backpack(max_w, data)
+    max_value, result = backpack.solve(), []  # добавить возвращение списка id объектов, взятых в рюкзак
+    print(f"Максимальная ценность рюкзака: {max_value}")
+    print(f"Объекты в рюкзаке: {result}")
